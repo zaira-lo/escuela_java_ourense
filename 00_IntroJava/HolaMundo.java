0cc63adebd74f354ed3af9b91cc080216437635d
Zaira <zairalopezarias@gmail.com>
2019-11-06 15:52:03 +0100

    Empezando: HolaMundo.java

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,9 @@
+class HolaMundo{
+	public static void main(String[] args){
+		byte unByte = 127;
+		int unEntero = 1000;
+		System.out.println("- Byte: " + unByte);
+		System.out.println("- Int: " + unEntero);
+		//System.out.println("- Int: " + unEntero);
+	}
+}
