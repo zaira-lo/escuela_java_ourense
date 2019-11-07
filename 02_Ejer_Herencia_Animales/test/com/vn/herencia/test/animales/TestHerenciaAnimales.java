93806299e55894914b3833cc4e2b3d425ddce70d
Zaira <zairalopezarias@gmail.com>
2019-11-07 17:49:56 +0100

    Añadir JUnit para testear el proyecto

---
+++ 02_Ejer_Herencia_Animales/test/com/vn/herencia/test/animales/TestHerenciaAnimales.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.herencia.test.animales;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class TestHerenciaAnimales {
+    
+    public TestHerenciaAnimales() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
