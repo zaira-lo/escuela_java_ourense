15250cb04e1cc7e15e25894e3e636917fd2b1371
Aurea <aurea.mene.h@gmail.com>
2019-11-22 17:51:28 +0100

    Creación DaoUsuarioList

---
+++ 06_ModeloUsuarioBBDD/src/com/vn/main/dao/DaoUsuarioList.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.main.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author Equipo 13
+ */
+public class DaoUsuarioList implements IDaoUsuario{
+    
+    private List<Usuario> listaUsuario;
+
+    @Override
+    public Usuario obtenerPorId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Usuario obtenerPorEmail(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Usuario crear(Usuario objetoNuevo) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Usuario obtenerPorIndice(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Usuario modificar(int index, Usuario objConDatosNuevo) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Usuario objConDatosNuevo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ 06_ModeloUsuarioBBDD/src/com/vn/main/dao/IDaoUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.main.dao;
+
+/**
+ *
+ * @author Equipo 13
+ */
+public interface IDaoUsuario extends IGenericDao<Usuario>{
+    
+    public Usuario obtenerPorId(int id);
+    
+    public Usuario obtenerPorEmail(String email);
+}
