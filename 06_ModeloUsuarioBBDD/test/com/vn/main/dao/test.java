901c48bb2ef8f62111a2acfe9a0809d92848230a
Zaira <zairalopezarias@gmail.com>
2019-11-25 09:16:50 +0100

    Crear junit

---
+++ 06_ModeloUsuarioBBDD/test/com/vn/main/dao/test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.main.dao;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class test {
+    
+    public test() {
+    }
+    
+
+    // @Test
+    // public void hello() {}
+}
