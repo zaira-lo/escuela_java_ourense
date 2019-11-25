cb49ffffeb41aa6e6ba9be36ede6407e9dfdaa43
Zaira <zairalopezarias@gmail.com>
2019-11-25 17:28:16 +0100

    Modificaciones en las expresiones regulares

--- 06_ModeloUsuarioBBDD/src/com/vn/main/dao/ServicioUsuarios.java
+++ 06_ModeloUsuarioBBDD/src/com/vn/main/dao/ServicioUsuarios.java
@@ -17,42 +17,40 @@ import java.util.regex.Pattern;
  *
  * @author Equipo 13
  */
-public class ServicioUsuarios{
+public class ServicioUsuarios {
 
     UsuariosDAO daoUsu;
 
     public ServicioUsuarios() {
         daoUsu = new UsuariosDAO();
     }
-   
+
     /**
-     * Función para validar usuarios:
-     * primero comprobar que ninguno de los parámetros es nulo,
-     * segundo comprobar que tienen una longitud mínima
-     * despues comprobar con expresiones regulares
-     * por ultimo comprobar que edad sea mayor que 12
-     * 
+     * Función para validar usuarios: primero comprobar que ninguno de los
+     * parámetros es nulo, segundo comprobar que tienen una longitud mínima
+     * despues comprobar con expresiones regulares por ultimo comprobar que edad
+     * sea mayor que 12
+     *
      * @param email
      * @param password
      * @param nombre
      * @param edad
-     * @return 
+     * @return
      */
     public Usuario crearUsuarioValido(String email, String password, String nombre, String edad) {
-        Pattern patternEmail = Pattern
-                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        Pattern patternNombre = Pattern
-                .compile("\"^[A-Z][a-z]+$\"");
-        Matcher matherEmail = patternEmail.matcher(email);
-        Matcher matherNombre = patternNombre.matcher(nombre);
-        
-        if (email != null && password != null && nombre != null && edad != edad) {
+
+        if (email != null && password != null && nombre != null && edad != null) {
             if (email.length() >= 3
                     && nombre.length() > 1
                     && password.length() >= 4
                     && !"".equals(edad)) {
+                String regexEmail = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+                Pattern patternEmail = Pattern.compile(regexEmail);
 
+                Pattern patternNombre = Pattern
+                        .compile("^[A-Z][a-z0-9]+ ?[A-Za-z0-9]*$");
+                Matcher matherEmail = patternEmail.matcher(email);
+                Matcher matherNombre = patternNombre.matcher(nombre);
                 if (matherEmail.find() == false) {
                     System.out.println("Debe tener formato de correo electrónico");
                     return null;
@@ -72,38 +70,39 @@ public class ServicioUsuarios{
                         System.out.println("La edad no se puede parsear a int: " + edad);
                         return null;
                     }
-                }                
-            }else{
+                }
+            } else {
                 System.out.println("Hay un campo que no cumple la longitud");
                 return null;
             }
-        }else{
+        } else {
             System.out.println("No admite NULOS");
             return null;
         }
     }
-    
-    
+
     /**
-     * 
+     *
      * @param email
      * @param password
      * @param nombre
      * @param edad
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
     public Usuario crear(String email, String password, String nombre, String edad) throws Exception {
         Usuario nuevoUsu = crearUsuarioValido(email, password, nombre, edad);
         if (nuevoUsu != null) {
-            daoUsu.crear(nuevoUsu);
+            nuevoUsu = daoUsu.crear(nuevoUsu);
             return nuevoUsu;
         }
         return null;
     }
+
     public Usuario leerUno(int id) {
         return daoUsu.obtenerPorId(id);
     }
+
     public Usuario leerUno(String email) {
         return daoUsu.obtenerPorEmail(email);
     }
